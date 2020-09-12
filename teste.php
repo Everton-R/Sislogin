82b09cdc3b284bc500e04d18ad632d26251b6c59
Everton-R <71192275+Everton-R@users.noreply.github.com>
2020-09-12 18:36:45 -0300

    teste

--- teste.php
+++ teste.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Teste Git</title>
+	<style type="text/css">
+		.h2{
+			background-color: "green";
+			width: 50px;
+			height: 20px;
+			font-size: 20px;
+			border: 2px dashed yellow;
+			border-radius: 4px;
+			
+		}
+	</style>
 </head>
 <body>
-	
+	<h2>teste</h2>
 </body>
 </html>
